d088e9469a20c50e66d89b38268db160579f57f3
ArtZymeri <132089036+ArtZymeri@users.noreply.github.com>
2023-11-28 15:58:44 +0100

    Applied changes on PC and fixed the Authenticator Checker

--- client/src/components/AuthenticatorChecker.js
+++ client/src/components/AuthenticatorChecker.js
@@ -1,22 +1,18 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const AuthenticatorChecker = ({ children }) => {
   const router = useRouter();
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const authenticated = localStorage.getItem('authenticated');
+  if (typeof window !== "undefined") {
+    const authenticated = localStorage.getItem("authenticated");
 
-      if (!authenticated) {
-        router.push('/auth/login');
-      }
+    if (!authenticated) {
+      router.push("/auth/login");
     } else {
-        return <>{children}</>;
+      return <>{children}</>;
     }
-  }, [router]);
-
-  
+  }
 };
 
 export default AuthenticatorChecker;
