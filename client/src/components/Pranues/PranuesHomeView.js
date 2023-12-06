6326c5f178d45f08ee289d5a3f804be8aa48373f
ArtZymeri <132089036+ArtZymeri@users.noreply.github.com>
2023-12-06 22:19:13 +0100

    Changes on Pranues Home UI

--- client/src/components/Pranues/PranuesHomeView.js
+++ client/src/components/Pranues/PranuesHomeView.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "@/styling/global.css";
 import "@/styling/Pranues/pranueshome.css";
 import axios from "axios";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const PranuesHomeView = () => {
   const [isClient, setIsClient] = useState(false);
@@ -23,6 +24,15 @@ const PranuesHomeView = () => {
             return (
               <div className="pranues-home-latestproducts-child shadow-one">
                 <img src={product.photo} />
+                <div className="pranues-home-latestproducts-child-text">
+                  <div className="pranues-home-latestproducts-child-text-up">
+                    <h4>{product.name}</h4>
+                    <span style={{ fontSize: "25px" }}>{product.price}€</span>
+                  </div>
+                  <button>
+                    Shto në shportë <ShoppingCartIcon sx={{ height: "20px" }} />
+                  </button>
+                </div>
               </div>
             );
           })}
