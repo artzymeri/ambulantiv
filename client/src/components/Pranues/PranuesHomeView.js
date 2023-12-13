da0f73c30cb03ad3a03c6dc8a2766a1c1cdb6343
ArtZymeri <132089036+ArtZymeri@users.noreply.github.com>
2023-12-13 19:47:13 +0100

    Added the same feature to the HomeProductsView

--- client/src/components/Pranues/PranuesHomeView.js
+++ client/src/components/Pranues/PranuesHomeView.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import "@/styling/global.css";
 import "@/styling/Pranues/pranueshome.css";
 import axios from "axios";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import ProductCard from "../ProductCard";
+import { Snackbar } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
 
 const PranuesHomeView = () => {
   const [isClient, setIsClient] = useState(false);
@@ -19,27 +20,57 @@ const PranuesHomeView = () => {
 
   const updateLocalStorage = (newArray) => {
     localStorage.setItem("cartProducts", JSON.stringify(newArray));
+    setSnackbarData({
+      title: "success",
+      message: "Produkti u shtua në shportë",
+    });
+    setSnackbarOpen(true);
+  };
+
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarData, setSnackbarData] = useState({ title: "", message: "" });
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackbarOpen(false);
   };
 
   return (
     isClient && (
-      <div className="pranues-home-parent">
-        <div className="pranues-home-latestproducts b-25 shadow-one">
-          {productsData
-            .slice()
-            .reverse()
-            .map((product) => {
-              return (
-                <ProductCard
-                  product={product}
-                  updateLocalStorage={updateLocalStorage}
-                />
-              );
-            })}
+      <>
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={2500}
+          onClose={handleSnackbarClose}
+        >
+          <MuiAlert
+            elevation={6}
+            variant="filled"
+            severity={snackbarData.title.toLowerCase()}
+          >
+            {snackbarData.message}
+          </MuiAlert>
+        </Snackbar>
+        <div className="pranues-home-parent">
+          <div className="pranues-home-latestproducts b-25 shadow-one">
+            {productsData
+              .slice()
+              .reverse()
+              .map((product) => {
+                return (
+                  <ProductCard
+                    product={product}
+                    updateLocalStorage={updateLocalStorage}
+                  />
+                );
+              })}
+          </div>
+          <div className="pranues-home-child b-25 shadow-one"></div>
+          <div className="pranues-home-child b-25 shadow-one"></div>
         </div>
-        <div className="pranues-home-child b-25 shadow-one"></div>
-        <div className="pranues-home-child b-25 shadow-one"></div>
-      </div>
+      </>
     )
   );
 };
