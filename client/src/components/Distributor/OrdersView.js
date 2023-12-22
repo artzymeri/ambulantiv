fbf2b9d469b18e76f0de84bebc0978f4b2898592
Art Zymeri <art.zymeri@hotelkeyapp.com>
2023-12-22 07:27:06 +0100

    Removed the invoice template completely

--- client/src/components/Distributor/OrdersView.js
+++ client/src/components/Distributor/OrdersView.js
@@ -5,16 +5,7 @@ import axios from "axios";
 import OrderItem from "./OrdersActiveItem";
 import "@/styling/Pranues/ordersview.css";
 import { Button } from "@mui/material";
-import { OutputType } from "jspdf-invoice-template";
-import dynamic from "next/dynamic";
 
-const jsPDFInvoiceTemplate = dynamic(
-  () => import("jspdf-invoice-template/dist/index"),
-  {
-    ssr: false,
-  }
-);
-import stateStorage from "@/store";
 
 const OrdersView = () => {
   const [isClient, setIsClient] = useState(false);
@@ -33,163 +24,163 @@ const OrdersView = () => {
       });
   }, []);
 
-  const generatePDFs = async () => {
-    if (typeof window !== "undefined") {
-      for (const order of ordersList) {
-        try {
-          const res = await axios.post(
-            `http://localhost:8080/completeorder/${order.id}`
-          );
-          const { title, message } = res.data;
-          console.log(title, message);
+  // const generatePDFs = async () => {
+  //   if (typeof window !== "undefined") {
+  //     for (const order of ordersList) {
+  //       try {
+  //         const res = await axios.post(
+  //           `http://localhost:8080/completeorder/${order.id}`
+  //         );
+  //         const { title, message } = res.data;
+  //         console.log(title, message);
 
-          function formatDateString(dateString) {
-            const dateObject = new Date(dateString);
+  //         function formatDateString(dateString) {
+  //           const dateObject = new Date(dateString);
 
-            const formattedDate = dateObject.toLocaleString("en-US", {
-              month: "2-digit",
-              day: "2-digit",
-              year: "numeric",
-              hour: "2-digit",
-              minute: "2-digit",
-              second: "2-digit",
-              hour12: true,
-            });
+  //           const formattedDate = dateObject.toLocaleString("en-US", {
+  //             month: "2-digit",
+  //             day: "2-digit",
+  //             year: "numeric",
+  //             hour: "2-digit",
+  //             minute: "2-digit",
+  //             second: "2-digit",
+  //             hour12: true,
+  //           });
 
-            return formattedDate;
-          }
+  //           return formattedDate;
+  //         }
 
-          var props = {
-            outputType: OutputType.Save,
-            returnJsPDFDocObject: true,
-            fileName: `Fatura ${order.productName} ${order.createdAt}.pdf`,
-            orientationLandscape: false,
-            compress: true,
-            logo: {
-              src: "/logoPdf.png",
-              type: "PNG", //optional, when src= data:uri (nodejs case)
-              width: 53.33, //aspect ratio = width/height
-              height: 53.33,
-              margin: {
-                top: -10, //negative or positive num, from the current position
-                left: 0,
-              },
-            },
-            // stamp: {
-            //   inAllPages: true, //by default = false, just in the last page
-            //   src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
-            //   type: "JPG", //optional, when src= data:uri (nodejs case)
-            //   width: 20, //aspect ratio = width/height
-            //   height: 20,
-            //   margin: {
-            //     top: 0, //negative or positive num, from the current position
-            //     left: 0, //negative or positive num, from the current position
-            //   },
-            // },
-            business: {
-              name: `${order.productDistributor}`,
-              address: "Adresa e Biznesit",
-              phone: "(+383) 49 111 222",
-              email: "distributor@gmail.com",
-            },
-            contact: {
-              label: "Fatura lëshuar për:",
-              name: `${order.productClient}`,
-              address: "Albania, Tirane, Astir",
-              phone: "(+355) 069 22 22 222",
-              email: "client@email.al",
-            },
-            invoice: {
-              label: "Data e lëshimit",
-              num: "",
-              invGenDate: formatDateString(order.createdAt),
-              headerBorder: false,
-              tableBodyBorder: false,
-              header: [
-                {
-                  title: "#",
-                  style: {
-                    width: 8,
-                  },
-                },
-                {
-                  title: "Emri i produktit",
-                  style: {
-                    width: 40,
-                  },
-                },
-                {
-                  title: "Distributori",
-                  style: {
-                    width: 30,
-                  },
-                },
-                { title: "Çmimi" },
-                { title: "Sasia" },
-                { title: "Pesha" },
-                { title: "Totali" },
-              ],
-              table: Array.from(Array(1), (item, index) => [
-                order.id,
-                order.productName,
-                order.productDistributor,
-                order.productPrice,
-                order.productQuantity,
-                order.productWeight,
-                order.productTotalPrice,
-              ]),
-              additionalRows: [
-                {
-                  col1: "Totali:",
-                  col2: order.productTotalPrice,
-                  col3: "EURO",
-                  style: {
-                    fontSize: 14, //optional, default 12
-                  },
-                },
-                {
-                  col1: "VAT:",
-                  col2: "20",
-                  col3: "%",
-                  style: {
-                    fontSize: 10, //optional, default 12
-                  },
-                },
-                {
-                  col1: "SubTotali:",
-                  col2: (parseFloat(order.productTotalPrice) * 0.8).toString(),
-                  col3: "EURO",
-                  style: {
-                    fontSize: 10, //optional, default 12
-                  },
-                },
-              ],
-            },
-            footer: {
-              text: "Fatura përdoret vetëm nga personat konkretë të përfshirë në marrëveshje.",
-            },
-            pageEnable: true,
-            pageLabel: "Page ",
-          };
+  //         var props = {
+  //           outputType: OutputType.Save,
+  //           returnJsPDFDocObject: true,
+  //           fileName: `Fatura ${order.productName} ${order.createdAt}.pdf`,
+  //           orientationLandscape: false,
+  //           compress: true,
+  //           logo: {
+  //             src: "/logoPdf.png",
+  //             type: "PNG", //optional, when src= data:uri (nodejs case)
+  //             width: 53.33, //aspect ratio = width/height
+  //             height: 53.33,
+  //             margin: {
+  //               top: -10, //negative or positive num, from the current position
+  //               left: 0,
+  //             },
+  //           },
+  //           // stamp: {
+  //           //   inAllPages: true, //by default = false, just in the last page
+  //           //   src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
+  //           //   type: "JPG", //optional, when src= data:uri (nodejs case)
+  //           //   width: 20, //aspect ratio = width/height
+  //           //   height: 20,
+  //           //   margin: {
+  //           //     top: 0, //negative or positive num, from the current position
+  //           //     left: 0, //negative or positive num, from the current position
+  //           //   },
+  //           // },
+  //           business: {
+  //             name: `${order.productDistributor}`,
+  //             address: "Adresa e Biznesit",
+  //             phone: "(+383) 49 111 222",
+  //             email: "distributor@gmail.com",
+  //           },
+  //           contact: {
+  //             label: "Fatura lëshuar për:",
+  //             name: `${order.productClient}`,
+  //             address: "Albania, Tirane, Astir",
+  //             phone: "(+355) 069 22 22 222",
+  //             email: "client@email.al",
+  //           },
+  //           invoice: {
+  //             label: "Data e lëshimit",
+  //             num: "",
+  //             invGenDate: formatDateString(order.createdAt),
+  //             headerBorder: false,
+  //             tableBodyBorder: false,
+  //             header: [
+  //               {
+  //                 title: "#",
+  //                 style: {
+  //                   width: 8,
+  //                 },
+  //               },
+  //               {
+  //                 title: "Emri i produktit",
+  //                 style: {
+  //                   width: 40,
+  //                 },
+  //               },
+  //               {
+  //                 title: "Distributori",
+  //                 style: {
+  //                   width: 30,
+  //                 },
+  //               },
+  //               { title: "Çmimi" },
+  //               { title: "Sasia" },
+  //               { title: "Pesha" },
+  //               { title: "Totali" },
+  //             ],
+  //             table: Array.from(Array(1), (item, index) => [
+  //               order.id,
+  //               order.productName,
+  //               order.productDistributor,
+  //               order.productPrice,
+  //               order.productQuantity,
+  //               order.productWeight,
+  //               order.productTotalPrice,
+  //             ]),
+  //             additionalRows: [
+  //               {
+  //                 col1: "Totali:",
+  //                 col2: order.productTotalPrice,
+  //                 col3: "EURO",
+  //                 style: {
+  //                   fontSize: 14, //optional, default 12
+  //                 },
+  //               },
+  //               {
+  //                 col1: "VAT:",
+  //                 col2: "20",
+  //                 col3: "%",
+  //                 style: {
+  //                   fontSize: 10, //optional, default 12
+  //                 },
+  //               },
+  //               {
+  //                 col1: "SubTotali:",
+  //                 col2: (parseFloat(order.productTotalPrice) * 0.8).toString(),
+  //                 col3: "EURO",
+  //                 style: {
+  //                   fontSize: 10, //optional, default 12
+  //                 },
+  //               },
+  //             ],
+  //           },
+  //           footer: {
+  //             text: "Fatura përdoret vetëm nga personat konkretë të përfshirë në marrëveshje.",
+  //           },
+  //           pageEnable: true,
+  //           pageLabel: "Page ",
+  //         };
 
-          const pdfObject = jsPDFInvoiceTemplate(props);
-          console.log(order);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-      axios
-        .get(
-          `http://localhost:8080/getactiveordersfromdistributor/${localStorage.getItem(
-            "companyname"
-          )}`
-        )
-        .then((res) => {
-          setOrdersList(res.data);
-          stateStorage.getDistributorActiveOrders();
-        });
-    }
-  };
+  //         const pdfObject = jsPDFInvoiceTemplate(props);
+  //         console.log(order);
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     }
+  //     axios
+  //       .get(
+  //         `http://localhost:8080/getactiveordersfromdistributor/${localStorage.getItem(
+  //           "companyname"
+  //         )}`
+  //       )
+  //       .then((res) => {
+  //         setOrdersList(res.data);
+  //         stateStorage.getDistributorActiveOrders();
+  //       });
+  //   }
+  // };
 
   return (
     isClient && (
