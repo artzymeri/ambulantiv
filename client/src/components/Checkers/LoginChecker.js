6d938f98c581fc07d8389129c8c6431d52c4fc49
Art Zymeri <art.zymeri@hotelkeyapp.com>
2024-01-08 22:28:01 +0100

    Fully responsiveness achieved

--- client/src/components/Checkers/LoginChecker.js
+++ client/src/components/Checkers/LoginChecker.js
@@ -1,5 +1,4 @@
 import Cookies from "js-cookie";
-import Head from "next/head";
 import { useRouter } from "next/router";
 
 const LoginChecker = ({ children }) => {
@@ -11,44 +10,12 @@ const LoginChecker = ({ children }) => {
       Cookies.get("distributorToken") !== undefined;
     const authenticatedPranues = Cookies.get("pranuesToken") !== undefined;
 
-    const logout = () => {
-      if (authenticatedAdmin) {
-        Cookies.remove("adminToken");
-        localStorage.removeItem("userId");
-        window.location.reload();
-      } else if (authenticatedDistributor) {
-        Cookies.remove("distributorToken");
-        localStorage.removeItem("userId");
-        localStorage.removeItem("emailaddress");
-        localStorage.removeItem("phonenumber");
-        localStorage.removeItem("companyname");
-        localStorage.removeItem("companyAddress");
-        window.location.reload();
-      } else if (authenticatedPranues) {
-        Cookies.remove("pranuesToken");
-        localStorage.removeItem("userId");
-        localStorage.removeItem("emailaddress");
-        localStorage.removeItem("phonenumber");
-        localStorage.removeItem("companyname");
-        localStorage.removeItem("companyAddress");
-        window.location.reload();
-      }
-    };
-
     if (
       authenticatedPranues ||
       authenticatedAdmin ||
       authenticatedDistributor
     ) {
-      return (
-        <>
-          <Head>
-            <link rel="icon" href="/e-commerceKosovaLogo.png" />
-          </Head>
-          <h1>You are already logged in</h1>
-          <button onClick={logout}>logout</button>
-        </>
-      );
+      router.push("/");
     } else {
       return <>{children}</>;
     }
