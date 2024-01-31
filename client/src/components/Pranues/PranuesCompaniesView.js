6be70bcffab1b02af573076a13179066b84fdd33
Art Zymeri <art.zymeri@hotelkeyapp.com>
2024-01-31 08:57:43 +0100

    Added search functionality for companies view on pranues

--- client/src/components/Pranues/PranuesCompaniesView.js
+++ client/src/components/Pranues/PranuesCompaniesView.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import "@/styling/global.css";
 import "@/styling/Pranues/pranuescompanies.css";
 import axios from "axios";
-import { Storage } from "@mui/icons-material";
+import { Search, Storage } from "@mui/icons-material";
 import { useRouter } from "next/router";
+import { TextField, Tooltip } from "@mui/material";
+import "@/styling/productsnavbar.css";
 
 const PranuesCompaniesView = () => {
   const [isClient, setIsClient] = useState(false);
@@ -25,6 +27,16 @@ const PranuesCompaniesView = () => {
         setLoading(false);
       });
   }, []);
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredCompanies = useMemo(() => {
+    return distributorsList.filter((distributor) =>
+      distributor?.companyname
+        ?.toLowerCase()
+        .includes(searchQuery.toString().toLowerCase())
+    );
+  }, [searchQuery]);
 
   return (
     isClient &&
@@ -58,31 +70,62 @@ const PranuesCompaniesView = () => {
             <Storage sx={{ marginBottom: "2px" }} />
             Kompanitë
           </div>
+          <div
+            style={{
+              width: "100%",
+              background: "white",
+              display: "flex",
+              gap: "10px",
+              border: "1px solid lightgray",
+              borderTop: "0px",
+              borderLeft: "0px",
+              borderRight: "0px",
+              flexShrink: "0",
+              zIndex: "999",
+              height: "70px",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "1.17em",
+              fontWeight: "800",
+              color: "rgb(130, 30, 30)",
+            }}
+          >
+            <div className="products-navbar-search-container">
+              <Search sx={{ color: "gray" }} />
+              <input
+                onChange={(e) => setSearchQuery(e.target.value)}
+                type="text"
+                placeholder="Kërko produktet"
+                value={searchQuery}
+              />
+            </div>
+          </div>
           <div className="pranues-companies-parent">
-            {distributorsList && distributorsList.length > 0
-              ? distributorsList.map((distributor, index) => {
+            {filteredCompanies && filteredCompanies.length > 0
+              ? filteredCompanies.map((distributor, index) => {
                   return (
-                    <div
-                      key={index}
-                      className="pranues-companies-item"
-                      onClick={() => {
-                        router.push({
-                          pathname: "/pranues/products/company",
-                          query: {
-                            companyname: distributor.companyname,
-                          },
-                        });
-                      }}
-                    >
-                      {distributor.companyLogo &&
-                      distributor.companyLogo.length > 4 ? (
-                        <img src={distributor.companyLogo} />
-                      ) : (
-                        <h3 style={{ color: "darkslategray" }}>
-                          {distributor.companyname}
-                        </h3>
-                      )}
-                    </div>
+                    <Tooltip key={index} title={distributor.companyname}>
+                      <div
+                        className="pranues-companies-item"
+                        onClick={() => {
+                          router.push({
+                            pathname: "/pranues/products/company",
+                            query: {
+                              companyname: distributor.companyname,
+                            },
+                          });
+                        }}
+                      >
+                        {distributor.companyLogo &&
+                        distributor.companyLogo.length > 4 ? (
+                          <img src={distributor.companyLogo} />
+                        ) : (
+                          <h3 style={{ color: "darkslategray" }}>
+                            {distributor.companyname}
+                          </h3>
+                        )}
+                      </div>
+                    </Tooltip>
                   );
                 })
               : null}
